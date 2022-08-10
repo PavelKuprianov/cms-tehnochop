31e6f5e1ebf529e05bc12258e0f9760464a9eb3b
Pavel Kuprianov <97345976+PavelKuprianov@users.noreply.github.com>
2022-08-10 23:09:37 +0300

    first

---
+++ technoshop-cms/script/index.js
@@ -0,0 +1,21 @@
+import { modalBtn, modal } from './elems.js';
+import { formController } from './formController.js';
+import { modalController } from './modalController.js';
+import { previewController } from './previewController.js';
+import { tableController } from './tableController.js';
+
+
+const init = () => {
+  modalController({
+    modal,
+    btn: modalBtn,
+    classOpen: 'd-block',
+    classClose: 'btn-close',
+  });
+
+  previewController();
+  tableController();
+  formController();
+}
+
+init();
